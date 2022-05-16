4d52387046373f4084cd4a832ffc49102ee890fc
Bruce Anderson <anderson.bruce.b@gmail.com>
2022-05-16 07:09:17 -0400

    init

--- src/handler.ts
+++ src/handler.ts
@@ -1,7 +1,9 @@
 import {key} from './key';
 export async function handleRequest(request: Request): Promise<Response> {
-  const url = `https://api.nasa.gov/planetary/apod?api_key=${key}`;
+  const reduced = request.url.replace("https://", "");
+  const idxOfSlash = reduced.indexOf("/");
+  const rest = reduced.substring(idxOfSlash + 1);
+  const url = `https://api.nasa.gov/${rest}?api_key=${key}`;
   const response = await fetch(url);
   return response;
-  //return new Response(`request method: ${request.method}`)
 }
